e3b878a5cd7d5d55e60642e1bdb61ede70849365
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-04-10 21:59:11 -0400

    city loc, current weather and 7-day forecast API calls

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-async function getWeatherData(city){
-    console.log("Button was clicked")
-    let myKey = process.env.WEATHER_API_KEY
-    let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`
-    console.log("URL: ",requestURL)
-    const response = await fetch(requestURL, {mode: 'cors'});
-    console.log(response)
-    const responseBody = await response.json();
-    console.log(responseBody);
+async function reportWeatherData(city){
+    console.log("Button was clicked");
+    let myKey = process.env.WEATHER_API_KEY;
+    let requestCityLocation = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${myKey}`
+    let cityLoc = await getData(requestCityLocation)
+    let [lat,lon] = [cityLoc[0].lat, cityLoc[0].lon]
+    console.log("lat: "+lat+" lon: "+lon)
+    let requestCurrentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
+    let requestWeekForecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${myKey}`;
+
+    let [weatherToday, weekForecast] = await Promise.all([getData(requestCurrentWeatherURL),getData(requestWeekForecastURL)]);
+    console.log(weatherToday);
+    console.log(weekForecast);
 }
 
-
+async function getData(myURL){
+    const response = await fetch(myURL, {mode: 'cors'});
+    //console.log(response)
+    const responseBody = await response.json();
+    console.log(myURL)
+    console.log(responseBody)
+    return responseBody;
+}
 
 function init(){
     console.log("MY KEY :",process.env.WEATHER_API_KEY); // remove this after you've confirmed it working
-    document.getElementById("searchBtn").addEventListener("click",function(){getWeatherData(document.getElementById("inputCity").value)});
+    document.getElementById("searchBtn").addEventListener("click",function(){reportWeatherData(document.getElementById("inputCity").value)});
 }
 
 window.onload = init;
