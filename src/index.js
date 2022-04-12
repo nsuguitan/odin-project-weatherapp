d01cdc97d51bd4bf6feb6724577238980f8a1925
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-04-12 15:17:45 -0400

    widget formatting and starting to implement logic for top left and right blocks

--- src/index.js
+++ src/index.js
@@ -25,25 +25,32 @@ async function reportWeatherData(city){
     console.log("my city: ", myCity)
     console.log("CityID: ", myCity[0].id)
 
+    
+
 
     //console.log("lat: "+lat+" lon: "+lon)
     let requestCurrentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
     let requestWeekForecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${myKey}`;
+    
 
-    let [weatherToday, weekForecast] = await Promise.all([getData(requestCurrentWeatherURL),getData(requestWeekForecastURL)], createWidget(myCity[0].id, myKey));
+    let [weatherToday, weekForecast] = await Promise.all([getData(requestCurrentWeatherURL),getData(requestWeekForecastURL)]);
+    let weatherIconId = weatherToday.weather[0].icon
+    console.log("Today's weather icon: ", weatherIconId)
+    let weatherIconURL = `http://openweathermap.org/img/wn/${weatherIconId}@2x.png`
+    createWidget(myCity[0],weatherIconURL); 
 
 }
 
-async function createWidget(cityid,myKey){
-    let script = document.createElement('script');
-    script.innerHTML = `
-    window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-    window.myWidgetParam.push({id: 15,cityid: ${cityid} ,appid: '${myKey}',units: 'imperial',containerid: 'openweathermap-widget-15'});
-    (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();`;
-    document.head.appendChild(script);
-    return console.log("widget created")
-}
+// async function createWidget(cityid,myKey){
+//     let script = document.createElement('script');
+//     script.innerHTML = `
+//     window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+//     window.myWidgetParam.push({id: 15,cityid: ${cityid} ,appid: '${myKey}',units: 'imperial',containerid: 'openweathermap-widget-15'});
+//     (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+//     var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();`;
+//     document.head.appendChild(script);
+//     return console.log("widget created")
+// }
 
 async function getData(myURL){
     const response = await fetch(myURL, {mode: 'cors'});
@@ -54,9 +61,24 @@ async function getData(myURL){
     return responseBody;
 }
 
+function createWidget(location,weatherIconURL){
+    //top left
+    console.log("Location:",location);
+    const widgetLocation = document.getElementById("reportLocation");
+    if(location.country ==='US'){
+        widgetLocation.innerHTML = location.name + ", "+ location.state+ ", "+location.country;
+    }
+    else{
+        widgetLocation.innerHTML = location.name + ", " + location.country;
+    }
+
+    //top right
+    const todayWeatherIcon = document.getElementById("reportLocation");
+}
 async function init(){
     console.log("MY KEY :",process.env.WEATHER_API_KEY); // remove this after you've confirmed it working
     document.getElementById("searchBtn").addEventListener("click",function(){reportWeatherData(document.getElementById("inputCity").value)});
+    
 }
 
 window.onload = init;
